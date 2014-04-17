f96c99073bef93fc57a3a9aef81437d3a4813574
bhaveen rana <brana@oxind.com>
2014-04-17 14:49:29 +0530

    -twig changes and css changes

--- Resources/public/js/search.js
+++ Resources/public/js/search.js
@@ -1,43 +1,91 @@
+$(document).ready(function() {
 
-/*
- * This file is part of the OxindFeedbackBundle package.
- *
- * (c) OxindFeedbackBundle <https://github.com/Oxind/OxindFeedbackBundle/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+    var pageCount = $("#pagecount").data('totalpage');
 
-/**
- * Description of FeedbackFilter
- *
- * @author Hardik Patel <hpatel@oxind.com>
- */
+    function searchAjax() {
+        var url = $("#feecback_filter").attr("action");
+        var data = $("#feecback_filter").serialize();
+        var searchText = $("#feecback_filter_search").attr("value");
+        $('#loader').show();
+        $.ajax({
+            type: "POST",
+            url: url, // Or your url generator like Routing.generate('discussion_create')
+            data: data,
+            dataType: "html",
+            success: function(data) {
+                $("#update_listing").html(data);
+                $('#loader').hide();
+
+                pageCount = $("#pagecount").data('totalpage');
+                currentPage = $("#feecback_filter_page").attr('value');
+                $("#next").show();
+                $("#prev").show();
+                if (pageCount < 1)
+                {
+                    $("#next").hide();
+                    $("#prev").hide();
+                }
+                if (currentPage == 0)
+                {
+                    $("#prev").hide();
+                }
+                if (currentPage == pageCount - 1)
+                {
+                    $("#next").hide();
+                }
+            }
+        });
+
+        return false;
+    }
+
+    $('#feecback_filter_statuses').change(function() {
+        $("#feecback_filter_page").attr('value', 0);
+        searchAjax();
+
+    });
+
+    $('#feecback_filter_search').on('input', function() {
+        $("#feecback_filter_page").attr('value', 0);
+        searchAjax();
+
+    });
 
 
-function loadSearchlist(feedbacktype_id, url)
-{
-    var xmlhttp;
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
+    if (pageCount > 1)
+    {
+        var currentPage = $("#feecback_filter_page").attr('value');
+        if (currentPage == 0)
+        {
+            $("#prev").hide();
+        }
+        $("#next").click(function(e) {
+            e.preventDefault();
+            pageCount = $("#pagecount").data('totalpage');
+            var pageValue = $("#feecback_filter_page").attr('value');
+            var pageNo = parseInt(pageValue);
+            pageNo++;
+            $("#feecback_filter_page").attr('value', pageNo);
+            searchAjax();
+        });
+
+        $("#prev").click(function(e) {
+            e.preventDefault();
+            pageCount = $("#pagecount").data('totalpage');
+            var pageValue = $("#feecback_filter_page").attr('value');
+            var pageNo = parseInt(pageValue);
+            if (pageNo > 0)
+            {
+                pageNo--;
+            }
+            $("#feecback_filter_page").attr('value', pageNo);
+            searchAjax();
+        });
     }
     else
-    {// code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function()
     {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
-        {
-            document.getElementById("feedback-list-container").parentNode.innerHTML = xmlhttp.responseText;
-        }
+        $("#next").hide();
+        $("#prev").hide();
     }
-    var query = document.getElementById('query').value;
-    var serverquery = encodeURI("?q=" + query + "&feedbacktype_id=" + feedbacktype_id);
 
-    xmlhttp.open("POST", url + serverquery, true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    xmlhttp.send();
-}
+});
